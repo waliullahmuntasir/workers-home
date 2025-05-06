1666f598fe6f5a2c8b0d37d826bcf077ce13e80e
Waliullah Muntasir <waliullah.muntasir@gmail.com>
2025-05-06 22:51:10 +0600

    complite

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {
+        fontFamily: {
+          body: ['Manrope', 'sans-serif'],
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
